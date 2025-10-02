bd305a4e60368addb665392afc2ac53be95e3962
Kyra <smy2qe@virginia.edu>
2025-10-02 15:44:00 -0400

    Add home screen page

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { View, Text, StyleSheet, Button } from "react-native";
+
+import HomeScreen from "./src/frontend/screens/HomeScreen";
 import SettingsScreen from "./src/frontend/screens/SettingsScreen";
 
 function GameModesScreen() {
@@ -17,6 +19,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
         <Tab.Screen name="Game Modes" component={GameModesScreen} />
         <Tab.Screen name="Statistics" component={StatisticsScreen} />
